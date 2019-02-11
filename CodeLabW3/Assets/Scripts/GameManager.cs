39ce566d0b1703bbe49c3adefb710ffd71466653
Siddarth <siddarthrgovindan@gmail.com>
2019-02-11 13:47:01 -0500

    Created Player cylinders

--- CodeLabW3/Assets/Scripts/GameManager.cs
+++ CodeLabW3/Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    private int score;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
