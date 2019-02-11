fbf15f0dfdd37f491e95ae6ba2c29f94cf7fbc77
Siddarth <siddarthrgovindan@gmail.com>
2019-02-11 14:05:26 -0500

    Created a public 'Score' property for the private variable 'score'

--- CodeLabW3/Assets/Scripts/GameManager.cs
+++ CodeLabW3/Assets/Scripts/GameManager.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
-    private int score;
-    
-    
+    private int score = 0;
+
+    public int Score
+    {
+        get { return score; }
+        set
+        {
+            Debug.Log("Score was set to: " + value);
+            score = value;
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +28,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Score = 7;
     }
+ 
 }
